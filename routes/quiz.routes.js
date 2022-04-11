d40296aeeec7d3fc048db7d33318252044a96b68
Rowena Schmidt <schmidt.rowena@gmail.com>
2022-04-11 11:02:23 +0200

    wihtout console.logs

--- routes/quiz.routes.js
+++ routes/quiz.routes.js
@@ -118,13 +118,10 @@ router.get("/own-friends-quizzes", isLoggedIn, csrfMiddleware, async (req, res) 
 
 router.get("/questions", isLoggedIn, csrfMiddleware, async (req, res) => {
   try {
-    console.log(req.query)
     let { quizId, category, difficulty } = req.query;
-    console.log(quizId)
     if (quizId !== "") {
       quizId = mongoose.Types.ObjectId(quizId);
       const questions = await Question.find({quiz: quizId});
-      console.log(questions)
       return res.json({ questions });
     } else {
       const response = await axios.get(`https://opentdb.com/api.php?amount=10&category=${category}&difficulty=${difficulty}&type=multiple`);
