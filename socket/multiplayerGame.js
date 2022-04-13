ff8f589a9a03f9b4d2c3603ea450f5ea0af036b3
Luiz Felipe dos Santos <luizfelipesantos.sc@gmail.com>
2022-04-13 14:51:04 +0200

    Added Try Catch and More Room Controls

--- socket/multiplayerGame.js
+++ socket/multiplayerGame.js
@@ -14,53 +14,102 @@ const initializeGame = (sio, socket) => {
     gameSocket.on("startGame", startGame);
     gameSocket.on("answerQuestion", answerQuestion);
     gameSocket.on("updateScore", updateScore);
-    gameSocket.on("disconnectGame", onDisconnect);
+    gameSocket.on("playerLeaveRoom", playerLeaveRoom);
+    gameSocket.on("deleteRoom", deleteRoom);
+    gameSocket.on("disconnect", onDisconnect);
 }
 
 function createNewGame(idData) {
-    const thisGameId = (( Math.random() * 100000 ) | 0).toString();
+    try {
+        const thisGameId = (( Math.random() * 100000 ) | 0).toString();
 
-    this.emit('newGameCreated', {gameId: thisGameId, mySocketId: this.id});
-
-    this.join(thisGameId);
-    usersRoom[thisGameId] = [{username: idData.username, score: 0}];
-
-    io.sockets.in(thisGameId).emit('playerJoinedRoom', { usersRoom: usersRoom[thisGameId]});
+        this.emit('newGameCreated', {gameId: thisGameId, mySocketId: this.id});
+    
+        this.join(thisGameId);
+        usersRoom[thisGameId] = [{username: idData.username, score: 0}];
+    
+        io.sockets.in(thisGameId).emit('playerJoinedRoom', { usersRoom: usersRoom[thisGameId], gameId: thisGameId});   
+    } catch (error) {
+        this.emit('error', {errorMessage: "Sorry, something went wrong!"});
+    }
 }
 
 function playerJoinsGame(idData) {
-    idData.mySocketId = this.id;
-
-    if (!usersRoom[idData.gameId]) return;
-
-    this.join(idData.gameId);
-
-    usersRoom[idData.gameId].push({username: idData.username, score: 0});
-    idData.usersRoom = usersRoom[idData.gameId];
-
-    io.sockets.in(idData.gameId).emit('playerJoinedRoom', idData);
+    try {
+        idData.mySocketId = this.id;
+        if (!(idData.gameId in usersRoom)) {
+            this.emit('error', {errorMessage: "Room not found!"});
+            return;
+        }
+    
+        this.join(idData.gameId);
+    
+        usersRoom[idData.gameId].push({username: idData.username, score: 0});
+        idData.usersRoom = usersRoom[idData.gameId];
+    
+        this.emit('mySocketId', {mySocketId: idData.mySocketId});
+    
+        io.sockets.in(idData.gameId).emit('playerJoinedRoom', idData);        
+    } catch (error) {
+        this.emit('error', {errorMessage: "Sorry, something went wrong!"});
+    }
 }
 
 function startGame(idData) {
-    idData.usersRoom = usersRoom[idData.gameId];
-    this.broadcast.in(idData.gameId).emit('gameStarted', idData);
+    try {
+        idData.usersRoom = usersRoom[idData.gameId];
+        this.broadcast.in(idData.gameId).emit('gameStarted', idData);            
+    } catch (error) {
+        this.emit('error', {errorMessage: "Sorry, something went wrong!"});
+    }
 }
 
 function answerQuestion(idData) {
-    io.sockets.in(idData.gameId).emit('playerAnswered', idData);
+    try {
+        io.sockets.in(idData.gameId).emit('playerAnswered', idData);        
+    } catch (error) {
+        this.emit('error', {errorMessage: "Sorry, something went wrong!"});
+    }
 }
 
 function updateScore(idData) {
-    usersRoom[idData.gameId].find(user => user.username === idData.username).score = idData.score;
-    idData.usersRoom = usersRoom[idData.gameId];
-    io.sockets.in(idData.gameId).emit('scoreUpdated', idData);
+    try {
+        usersRoom[idData.gameId].find(user => user.username === idData.username).score = idData.score;
+        idData.usersRoom = usersRoom[idData.gameId];
+        io.sockets.in(idData.gameId).emit('scoreUpdated', idData);  
+    } catch (error) {
+        this.emit('error', {errorMessage: "Sorry, something went wrong!"});
+    }
 }
 
-function onDisconnect(idData) {
-    this.leave(idData.gameId);
-    usersRoom[idData.gameId] = null;
-    const i = gamesInSession.indexOf(gameSocket);
-    gamesInSession.splice(i, 1);
+function playerLeaveRoom(idData) {
+    try {
+        this.leave(idData.gameId);
+        const i = usersRoom[idData.gameId].findIndex(user => user.username === idData.username);
+        usersRoom[idData.gameId].splice(i, 1);
+        idData.usersRoom = usersRoom[idData.gameId];
+        io.sockets.in(idData.gameId).emit('playerLeavedRoom', idData);   
+    } catch (error) {
+        this.emit('error', {errorMessage: "Sorry, something went wrong!"});
+    }
+}
+
+function deleteRoom(idData) {
+    try {
+        delete usersRoom[idData.gameId];
+        this.broadcast.in(idData.gameId).emit('roomDeleted');            
+    } catch (error) {
+        this.emit('error', {errorMessage: "Sorry, something went wrong!"});
+    }
+}
+
+function onDisconnect() {
+    try {
+        const i = gamesInSession.indexOf(gameSocket);
+        gamesInSession.splice(i, 1);            
+    } catch (error) {
+        this.emit('error', {errorMessage: "Sorry, something went wrong!"});
+    }
 }
 
 exports.initializeGame = initializeGame
